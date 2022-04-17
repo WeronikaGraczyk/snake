be822d656f6a7c52e985cec98f4c12dd6c21a07a
Weronika Graczyk <wercia01@onet.pl>
2022-04-17 22:15:53 +0200

    SnakeGame- Snake running

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -8,25 +8,25 @@ import java.util.Random;
 
 public class SnakeGame extends JPanel implements ActionListener {
 
-    static final int SCREEN_WIDTH=600;
-    static final int SCREEN_HEIGTH=600;
-    static final int UNIT_SIZE=25;
-    static final int UNITS=(SCREEN_HEIGTH*SCREEN_WIDTH)/UNIT_SIZE;
-    static final int DELAY=75;
-    final int x[]=new int[UNITS];
-    final int y[]=new int[UNITS];
-    int snakeParts=6;
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGTH = 600;
+    static final int UNIT_SIZE = 25;
+    static final int UNITS = (SCREEN_HEIGTH * SCREEN_WIDTH) / UNIT_SIZE;
+    static final int DELAY = 175;
+    final int[] x = new int[UNITS];
+    final int[] y = new int[UNITS];
+    int snakeParts = 6;
     int applesEaten;
     int appleX;
     int appleY;
-    char direction='R';
-    boolean running=false;
+    char direction = 'R';
+    boolean running = false;
     Timer timer;
     Random random;
 
     public SnakeGame() {
-        random=new Random();
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGTH));
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGTH));
         this.setBackground(Color.DARK_GRAY);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
@@ -35,34 +35,92 @@ public class SnakeGame extends JPanel implements ActionListener {
 
     private void startGame() {
         newApple();
-        running=true;
-        timer=new Timer(DELAY,this);
+        running = true;
+        timer = new Timer(DELAY, this);
         timer.start();
     }
-    public void paintComponent(Graphics g){
+
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
     }
-    public void draw(Graphics g){
-        for(int i=0;i<SCREEN_HEIGTH/UNIT_SIZE;i++){
-            g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGTH);
+
+    private void draw(Graphics g) {
+        for (int i = 0; i < SCREEN_HEIGTH / UNIT_SIZE; i++) {
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGTH);
         }
-        for(int i=0;i<SCREEN_WIDTH/UNIT_SIZE;i++){
-            g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+        for (int i = 0; i < SCREEN_WIDTH / UNIT_SIZE; i++) {
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+        }
+        g.setColor(new Color(255, 51, 51));
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for (int i = 0; i < snakeParts; i++) {
+            if (i == 0) {
+                g.setColor(new Color(33, 255, 92));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            } else {
+                g.setColor(new Color(45, 180, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
+    }
+
+    private void move() {
+        for (int i = snakeParts; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
+        }
+        switch (direction) {
+            case 'U' -> y[0] = y[0] - UNIT_SIZE;
+            case 'D' -> y[0] = y[0] + UNIT_SIZE;
+            case 'L' -> x[0] = x[0] - UNIT_SIZE;
+            case 'R' -> x[0] = x[0] + UNIT_SIZE;
         }
     }
 
     private void newApple() {
+        appleX = random.nextInt(SCREEN_WIDTH / UNIT_SIZE) * UNIT_SIZE;
+        appleY = random.nextInt(SCREEN_HEIGTH / UNIT_SIZE) * UNIT_SIZE;
     }
+
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (running) {
+            move();
+
+        }
+        repaint();
 
     }
-    public class MyKeyAdapter extends KeyAdapter{
-        @Override
-        public void keyPressed(KeyEvent e){
 
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()){
+                case KeyEvent.VK_RIGHT:
+                    if(direction!='L'){
+                        direction='R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if(direction!='D'){
+                        direction='U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if(direction!='U'){
+                        direction='D';
+                    }
+                    break;
+                case KeyEvent.VK_LEFT:
+                    if(direction!='R'){
+                        direction='L';
+                    }
+                    break;
+            }
         }
     }
 }
